5906ca83363caeb1c13d4c88478c45f53a83df59
Zie <ndlovuzibusiso@gmail.com>
2025-04-25 09:59:41 +0200

    modified outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 
 output "instance_id" {
-  value = aws_instance.example.id
+  value = aws_instance.ubuntu_vm.id
 }
 
 output "public_ip" {
-  value       = aws_instance.example.public_ip
+  value       = aws_instance.ubuntu_vm.public_ip
   description = "The public IP of the web server"
 }
 
 output "security_groups" {
-  value = [aws_instance.example.vpc_security_group_ids]
+  value = [aws_instance.ubuntu_vm.vpc_security_group_ids]
 }
